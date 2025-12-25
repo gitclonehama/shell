fd00951186577ae071a1f946206c7e8edea4ecc8
Hama Pashazadeh <pashazad@msu.edu>
2025-12-25 17:43:45 -0500

    Parse user input, relay error message

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  // Get user input
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  // Remove the trailing '\n'
+  command[strcspn(command, "\n")] = '\0';
+
+  printf("%s: command not found\n", command);
 
   return 0;
 }
