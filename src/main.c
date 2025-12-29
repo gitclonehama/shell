67c9e3e5db61272ee5f90bebc2036f075a936922
Hama Pashazadeh <pashazad@msu.edu>
2025-12-29 10:16:21 -0500

    Implement type function

--- src/main.c
+++ src/main.c
@@ -6,24 +6,50 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   char input[1024];
-  while (strcmp(input, "exit")) {
+  while (1) {
     printf("$ ");
 
     // Get user input
-    fgets(input, sizeof(input), stdin);
+    if (fgets(input, sizeof(input), stdin) == NULL)
+      break;
 
-    // Remove the trailing '\n'
+    // Remove trailing newline
     input[strcspn(input, "\n")] = '\0';
 
-    if (strcmp(input, "exit") == 0) {
-      return 0;
+    if (strcmp(input, "exit") == 0)
+      break;
+
+    // Parse command from input
+    char *space_ptr = strchr(input, ' ');
+    char command[1024];
+
+    if (space_ptr != NULL) {
+      size_t cmd_len = space_ptr - input;
+      strncpy(command, input, cmd_len);
+      command[cmd_len] = '\0';
+    } else {
+      // No space, whole input is command
+      strcpy(command, input);
     }
 
-    char *echo = strstr(input, "echo");
-    if (echo) {
-      printf("%s\n", echo + sizeof("echo"));
+    if (strcmp(command, "echo") == 0) {
+      if (space_ptr != NULL) {
+        printf("%s\n", space_ptr + 1);
+      } else {
+        printf("\n");
+      }
+    } else if (strcmp(command, "type") == 0) {
+      if (space_ptr != NULL) {
+        if (strcmp(space_ptr + 1, "echo") == 0 ||
+            strcmp(space_ptr + 1, "exit") == 0 ||
+            strcmp(space_ptr + 1, "type") == 0) {
+          printf("%s is a shell builtin\n", command);
+        } else {
+          printf("%s: command not found\n", command);
+        }
+      }
     } else {
-      printf("%s: command not found\n", input);
+      printf("%s: command not found\n", command);
     }
   }
 
